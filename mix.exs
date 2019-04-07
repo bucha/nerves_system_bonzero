86796ed0fa652c8dbcd39e30bf3196fc729a13ba
Alexander Buch <alexander.buch@cartware.de>
2019-04-07 22:55:58 +0200

    Added everything needed for Docker (and possibly even more)

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
-defmodule NervesSystemRpi0.MixProject do
+defmodule NervesSystemBonzero.MixProject do
   use Mix.Project
 
-  @app :nerves_system_rpi0
+  @app :nerves_system_bonzero
   @version Path.join(__DIR__, "VERSION")
            |> File.read!()
            |> String.trim()
@@ -34,9 +34,9 @@ defmodule NervesSystemRpi0.MixProject do
   defp nerves_package do
     [
       type: :system,
-      artifact_sites: [
-        {:github_releases, "nerves-project/#{@app}"}
-      ],
+#      artifact_sites: [
+#        {:github_releases, "nerves-project/#{@app}"}
+#      ],
       build_runner_opts: build_runner_opts(),
       platform: Nerves.System.BR,
       platform_config: [
@@ -67,7 +67,7 @@ defmodule NervesSystemRpi0.MixProject do
       maintainers: ["Frank Hunleth", "Justin Schneck"],
       files: package_files(),
       licenses: ["Apache 2.0"],
-      links: %{"Github" => "https://github.com/nerves-project/#{@app}"}
+      links: %{"Github" => "https://github.com/bucha/#{@app}"}
     ]
   end
 
